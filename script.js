8ea2e0a336109ccae0730f35713c7bfbd221fad5
KuznetsovaMaria <113997426+KuznetsovaMaria@users.noreply.github.com>
2024-05-10 23:24:45 +0500

    feat: Add prompt for number of squares and draw grid accordigly

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const gridContainer = document.getElementById('gridContainer');
 const btnCreateGrid = document.getElementById('btnCreateGrid');
 
+let numSquaresOnOneSide = prompt('How many squares on one side would you like me to draw in your grid?')
+let numSquaresTotal  = numSquaresOnOneSide**2;
+
 btnCreateGrid.addEventListener('click', () => {
-    if (gridContainer.lastChild) {
-        removeGrid();
+    if (gridContainer.hasChildNodes()) {
+        removeGrid(gridContainer);
     }
     drawGrid();
 });
 
 function drawGrid() {
-    for (let squareNum = 1; squareNum <= 25; squareNum++) {
+    for (let squaresNum = 1; squaresNum <= numSquaresTotal; squaresNum++) {
         drawSquare();
     }
 }
 
 function drawSquare() {
+    let flexPercentage = 100 / numSquaresOnOneSide;
     let square = document.createElement('div');
     square.classList.add('square');
+    square.style.flex = `1 1 ${flexPercentage}%`;
     gridContainer.appendChild(square);
 }
 
@@ -31,10 +36,9 @@ function changeSquareColor(square) {
     square.classList.add('squareColored');
 }
 
-function removeGrid() {
-    for (let squareNum = 25; squareNum > 0; squareNum--) {
+function removeGrid(parent) {
+    while(parent.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.lastChild);
-        console.log(squareNum);
     }
 }
 
